fb828ff636aeeb4b479483e4194d553894e9795a
HoutVouyIm <vouyimhout0962422747@gmail.com>
2024-03-13 16:47:31 +0700

    push to master

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Auto Complete</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/571a0c1ad3.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="search-box">
+        <div class="row">
+            <input type="text" name="" id="input-box" placeholder="Search anything"
+            autocomplete="off">
+            <button>
+                <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+    </div>
+</body>
+</html>
